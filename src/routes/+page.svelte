17589a089bed4406c5d4cd78578882e2bdc8aca0
Henil Malaviya <henilmalaviya06@gmail.com>
2023-10-25 16:10:49 +0530

    show output

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
 	import { trpc } from '$lib/trpc/client';
 
 	let input = '';
+	let output = '';
 
 	async function translate() {
 		const response = await trpc().translate.query({
@@ -11,10 +12,12 @@
 		});
 
 		console.log(response);
+		output = response;
 	}
 </script>
 
 <div class="w-full h-screen flex justify-center items-center flex-col gap-6">
 	<Input bind:value={input} class="border-black w-fit" />
 	<Button on:click={translate}>Translate</Button>
+	<div class="my-4">{output}</div>
 </div>
