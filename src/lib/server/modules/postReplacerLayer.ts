d65be75d428c30e9b709b2c53779fbf3aa7ddbb7
Henil Malaviya <henilmalaviya06@gmail.com>
2024-03-05 10:27:04 +0530

    temp

--- src/lib/server/modules/postReplacerLayer.ts
+++ src/lib/server/modules/postReplacerLayer.ts
@@ -13,7 +13,10 @@ const schema = {
 	}),
 
 	output: z.object({
-		result: z.string()
+		result: z.string(),
+		changes: z.array(z.object({
+
+		}))
 	})
 };
 
@@ -28,11 +31,49 @@ export async function postReplacerLayer(options: PostLayerOptions): Promise<Post
 
 	for (const search in database) {
 		const replacement = database[search];
+		const expression = new RegExp(search, 'gi');
+		const changes = {
+			old: {
+				startingIndex: -1,
+				endIndex: -1,
+			},
+			new: {
+				startingIndex: -1,
+				endIndex: -1,
+			},
+			from: search,
+			to: replacement
+		}
 
-		options.input = options.input.replace(new RegExp(search, 'gi'), replacement);
+
+		const isReplaceable = expression.test(options.input);
+
+		if (isReplaceable) {
+			const startingIndexOfOld = options.input.indexOf(search);
+			const endIndexOfOld = startingIndexOfOld + search.length;
+
+			changes.old.startingIndex = startingIndexOfOld;
+			changes.old.endIndex = endIndexOfOld;
+		}
+
+		options.input = options.input.replace(expression, replacement);
+
+		if (isReplaceable) {
+			const startingIndexOfNew = options.input.indexOf(replacement);
+			const endIndexOfNew = startingIndexOfNew + replacement.length;
+
+			changes.new.startingIndex = startingIndexOfNew;
+			changes.new.endIndex = endIndexOfNew;
+		}
+
+		if (isReplaceable) {
+			console.log("changes?", changes);
+		}
+
 	}
 
 	return {
-		result: options.input
+		result: options.input,
+		changes: []
 	};
 }
